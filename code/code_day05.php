3b5867a56b480e58c34ed921be840b14701d7505
Kenny Yeager <kenny.yeager@gmail.com>
2023-12-05 13:54:34 -0800

    day05,part2 stuck on memory usage

--- code/code_day05.php
+++ code/code_day05.php
@@ -9,8 +9,12 @@ relationships to the end, storing the data in another array. Finally, we'll comp
 values in that array to choose the lowest result for the final result.
 */
 
+# I'm not sure it's a memory leak at this point, though it may be
+# Still, I need to increase available memory again
+#ini_set("memory_limit", -1);
 # define the script-wide variables and arrays
 $seed_numbers = [];
+$expanded_seed_numbers = [];
 $seed_to_soil_maps = [];
 $soil_to_fertilizer_maps = [];
 $fertilizer_to_water_maps = [];
@@ -20,7 +24,6 @@ $temperature_to_humidity_maps = [];
 $humidity_to_location_maps = [];
 $current_category = "";
 $current_operating_array = [];
-$lowest_location = null;
 
 # open the input file and make it accessible
 $input_file = fopen("../inputs/input_day05.txt", "r");
@@ -85,25 +88,26 @@ $current_operating_array = null;
 # close the input file
 fclose($input_file);
 
+# in the name of not repeating myself, I've moved the foreach logic to a function
 # begin iterating through the seed -> soil -> fertilizer -> water -> light -> temperature -> humidity -> location logic
-foreach($seed_numbers as $seed){
-    $soil = check_within_range_and_return_mapped_value($seed, $seed_to_soil_maps);
-    $fertilizer = check_within_range_and_return_mapped_value($soil, $soil_to_fertilizer_maps);
-    $water = check_within_range_and_return_mapped_value($fertilizer, $fertilizer_to_water_maps);
-    $light = check_within_range_and_return_mapped_value($water, $water_to_light_maps);
-    $temperature = check_within_range_and_return_mapped_value($light, $light_to_temperature_maps);
-    $humidity = check_within_range_and_return_mapped_value($temperature, $temperature_to_humidity_maps);
-    $location = check_within_range_and_return_mapped_value($humidity, $humidity_to_location_maps);
-    #echo("for seed #" . $seed . ", the location match would be: " .  $location . "\n");
-    if($lowest_location == null || $location < $lowest_location){
-        $lowest_location = $location;
+$lowest_location_part1 = iterate_through_arrays($seed_numbers);
+
+# part two logic simply requires changing the seed numbers starting point
+# to use ranges rather than singular values; beyond that, the preceding works as is.
+
+for($i = 0; $i < count($seed_numbers); $i+=2){
+    $starting_number = $seed_numbers[$i];
+    $working_range = $seed_numbers[$i + 1];
+    for($j = 0; $j < $working_range; $j++){
+        $expanded_seed_numbers[] = $starting_number + $j;
     }
 }
-
+$lowest_location_part2 = iterate_through_arrays($expanded_seed_numbers);
 
 # output the results
 echo "The output: \n";
-echo "The seed with the lowest location number is: " . $lowest_location . "\n";
+echo "The seed with the lowest location number in part one is: " . $lowest_location_part1 . "\n";
+echo "The seed with the lowest location number in part two is: " . $lowest_location_part2 . "\n";
 
 # moving this to a function clears the need of nested foreach loops
 function check_within_range_and_return_mapped_value($source_value, $array_of_arrays){
@@ -119,6 +123,26 @@ function check_within_range_and_return_mapped_value($source_value, $array_of_arr
         }
     }
     return $destination_value;
+}
+
+function iterate_through_arrays($seed_array){
+    global $seed_to_soil_maps, $soil_to_fertilizer_maps, $fertilizer_to_water_maps;
+    global $water_to_light_maps, $light_to_temperature_maps, $temperature_to_humidity_maps, $humidity_to_location_maps;
+    $lowest_location = null;
+    foreach($seed_array as $seed){
+        $soil = check_within_range_and_return_mapped_value($seed, $seed_to_soil_maps);
+        $fertilizer = check_within_range_and_return_mapped_value($soil, $soil_to_fertilizer_maps);
+        $water = check_within_range_and_return_mapped_value($fertilizer, $fertilizer_to_water_maps);
+        $light = check_within_range_and_return_mapped_value($water, $water_to_light_maps);
+        $temperature = check_within_range_and_return_mapped_value($light, $light_to_temperature_maps);
+        $humidity = check_within_range_and_return_mapped_value($temperature, $temperature_to_humidity_maps);
+        $location = check_within_range_and_return_mapped_value($humidity, $humidity_to_location_maps);
+        #echo("for seed #" . $seed . ", the location match would be: " .  $location . "\n");
+        if($lowest_location == null || $location < $lowest_location){
+            $lowest_location = $location;
+        }
+    }
+    return $lowest_location;
 }
 
 ?>
