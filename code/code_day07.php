2454aec4cec6b7209d4553608e9b1a1b9b5f0460
Kenny Yeager <kenny.yeager@gmail.com>
2023-12-08 15:53:46 -0800

    started on day07 comments

--- code/code_day07.php
+++ code/code_day07.php
@@ -1,14 +1,20 @@
 <?php
 
 /*
-Based on the prompt...
+Based on the prompt, we're going to need to do a lot of array manipulation.
+There are probably simpler ways to handle the data as presented, but we need several
+data points about the "hands" being dealt, including the original order, the calculated
+"power" of the hand, the "bid" associated with the hand, and the rank.
+A lot of this can be done with PHP's sorting... once the data is in meaningfully
+sortable arrays.
 */
 
 # open the input file and make it accessible
-$input_file = fopen("../inputs/input_day0.txt", "r");
-$input_file = fopen("../inputs/sample_day0.txt", "r");
+#$input_file = fopen("../inputs/input_day07.txt", "r");
+$input_file = fopen("../inputs/sample_day07.txt", "r");
 
 #declare global variables
+
 
 # do stuff with the input file
 while(!feof($input_file)) {
